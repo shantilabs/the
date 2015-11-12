0b46e4256b1d96ee3ac455a10b5a27434c3fed9b
Maxim Oransky <maxim.oransky@gmail.com>
2015-11-13 01:59:31 +0300

    опечатка

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 function is_not_dot($s) { return $s[0] != '.'; }
-$dirs = array_filter('is_not_dot', array_filter('is_dir', scandir('.'));
+$dirs = array_filter('is_not_dot', array_filter('is_dir', scandir('.')));
 $dir = $dirs[array_rand($dirs)];
 header("Location: /$dir/");
